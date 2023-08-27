b038a30d959a8d46c08b3bee5a48e914e5156db8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-27 20:40:46 +0530

    added btn to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="font-extrabold text-9xl text-center">Hello World!</h1>;
+  return (
+    <>
+      <Button>Click Me!</Button>
+      <h1 className="font-extrabold text-9xl text-center">Hello World!</h1>
+    </>
+  );
 }
