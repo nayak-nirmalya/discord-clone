725f69e526d2bd0de488a6a1e73a62621ea26677
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-28 10:14:10 +0530

    added SignUp page

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LoginPage() {
-  return <div>LoginPage</div>;
-}
-
-export default LoginPage;
--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function RegisterPage() {
-  return <div>RegisterPage</div>;
-}
-
-export default RegisterPage;
