e3c0446c6822777238c4477d1b6f5397d6c76279
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-28 10:12:40 +0530

    added authMiddleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Discord Clone",
-  description:
-    "Discord Clone with Next.js, React.js, TailWindCSS & TypeScript.",
+  description: "Discord Clone with Next.js, React.js, TailWindCSS & TypeScript."
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={openSans.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={openSans.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
