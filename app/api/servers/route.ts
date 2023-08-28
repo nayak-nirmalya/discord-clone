bd39335fd665783591728994be1c91e7278bcee9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-28 20:03:56 +0530

    added POST route /servers

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import { MemberRole } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -9,6 +11,19 @@ export async function POST(req: Request) {
     const profile = await currentProfile();
 
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const server = await db.server.create({
+      data: {
+        profileId: profile.id,
+        name,
+        imageUrl,
+        inviteCode: uuidv4(),
+        channels: { create: [{ name: "general", profileId: profile.id }] },
+        members: { create: [{ profileId: profile.id, role: MemberRole.ADMIN }] }
+      }
+    });
+
+    return NextResponse.json(server);
   } catch (error) {
     console.error("[SERVERS_POST]", error);
     return new NextResponse("Internal Error", { status: 500 });
