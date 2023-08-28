5d4c9b2fa52b7a5530575c0287fe0eb3fadc9a2e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-28 13:26:13 +0530

    ask user to create server if not member

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
 import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
 
 export default async function SetupPage() {
   const profile = await initialProfile();
 
-  return <div>SetupPage</div>;
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
+  return <div>Create a Server.</div>;
 }
