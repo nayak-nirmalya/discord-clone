48471e4e610842465489b92bdabb80fcf89695a8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-28 17:09:35 +0530

    added uploadthing domail to nextconfig and added close btn

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
@@ -12,11 +14,28 @@ interface FileUploadProps {
 }
 
 export function FileUpload({ onChange, value, endpoint }: FileUploadProps) {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        onChange(res?.[0].fileUrl);
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => console.error(error.message)}
     />
