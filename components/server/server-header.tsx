4796ad95463d441a4356d2863c6dca927b4ed020
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 15:41:59 +0530

    show invite people btn if moderator

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,8 +2,15 @@
 
 import React from "react";
 import { MemberRole } from "@prisma/client";
+import { ChevronDown, UserPlus } from "lucide-react";
 
 import { ServerWithMembersWithProfiles } from "@/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -11,5 +18,25 @@ interface ServerHeaderProps {
 }
 
 export function ServerHeader({ server, role }: ServerHeaderProps) {
-  return <div>ServerHeader</div>;
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none" asChild>
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+          {server.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+        {isModerator && (
+          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
