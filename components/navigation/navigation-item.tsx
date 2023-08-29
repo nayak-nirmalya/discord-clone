ccf349231e415858ee6b5e2d04a6e24060f4b024
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 12:41:52 +0530

    show server image on nav sidebar

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -14,5 +14,29 @@ interface NavigationItemProps {
 }
 
 export function NavigationItem({ id, imageUrl, name }: NavigationItemProps) {
-  return <div>NavigationItem</div>;
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <button onClick={() => {}} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt="Channel" />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
 }
