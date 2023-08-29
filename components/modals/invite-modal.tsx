18cb912bd46a408c573e3ac8abc986c637cdc869
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 20:03:40 +0530

    added new hook: useOrigin

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import React from "react";
+import { Copy, RefreshCw } from "lucide-react";
 
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function InviteModal() {
   const { isOpen, onClose, type } = useModal();
@@ -21,14 +24,31 @@ export function InviteModal() {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Invite Friends
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
-        Invite Modal!
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="flex items-center mt-2 gap-x-2">
+            <Input
+              value="invite-link"
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+            />
+            <Button size="icon">
+              <Copy className="w-4 h-4" />
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
