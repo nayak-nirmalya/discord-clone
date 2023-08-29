0f3747859fc84471f46e4a8179aaf022495c862d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 09:51:20 +0530

    new component NavigationSidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function MainLayout() {
-  return <div>MainLayout</div>;
+export default async function MainLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <main className="md:pl-[72px] h-full">{children}</main>
+    </div>
+  );
 }
