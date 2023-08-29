b088ae319f3e956c9fe10bc01bea2b46ecd9f38a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 09:46:36 +0530

    added main layout

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ServerIdPage() {
+  return <div>ServerIDPage</div>;
+}
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainLayout() {
+  return <div>MainLayout</div>;
+}
