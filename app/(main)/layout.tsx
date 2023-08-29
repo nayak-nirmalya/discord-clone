c6ef2e1435569ea5745637dc221cc7d505fee363
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-29 10:11:06 +0530

    added shadcn-ui tooltip

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+
 export default async function MainLayout({
   children
 }: {
@@ -7,7 +9,9 @@ export default async function MainLayout({
 }) {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
       <main className="md:pl-[72px] h-full">{children}</main>
     </div>
   );
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
-export function NavigationSidebar() {
-  return <div>NavigationSidebar</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function NavigationSidebar() {
+  const profile = await currentProfile();
+
+  if (!profile) return redirect("/");
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  return (
+    <div className="space-y-4 flex flex-col h-full items-center text-primary w-full dark:bg-[#1e1f22] py-3">
+      Navigation Sidebar
+    </div>
+  );
 }
