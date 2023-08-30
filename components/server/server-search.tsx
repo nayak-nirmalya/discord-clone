4632f14df380374571e3a2de53148c0065bd485e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-30 20:30:06 +0530

    added serch btn

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { Search } from "lucide-react";
 
 interface ServerSearchProps {
   data: {
@@ -17,5 +18,18 @@ interface ServerSearchProps {
 }
 
 export function ServerSearch({ data }: ServerSearchProps) {
-  return <div>ServerSearch</div>;
+  return (
+    <>
+      <button className="group px-2 py-2 rounded-md items-center flex gap-x-2 w-full hover:bg-zinc-700/10 dark:bg-zinc-700/50 transition">
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">⌘</span>
+          <span className="text-lg">K</span>
+        </kbd>
+      </button>
+    </>
+  );
 }
