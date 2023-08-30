20ada942569e46f16853a41108014755e8106302
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-30 12:11:51 +0530

    added onKick fn

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -53,6 +53,26 @@ export function MembersModal() {
   const isModalOpen = isOpen && type === "members";
   const { server } = data as { server: ServerWithMembersWithProfiles };
 
+  const onKick = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId);
+
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: { serverId: server?.id }
+      });
+
+      const response = await axios.delete(url);
+
+      router.refresh();
+      onOpen("members", { server: response.data });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingId("");
+    }
+  };
+
   const onRoleChange = async (memberId: string, role: MemberRole) => {
     try {
       setLoadingId(memberId);
