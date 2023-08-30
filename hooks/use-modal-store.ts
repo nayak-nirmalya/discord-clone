5aa54dc2dc34b84c14168a5d0b549f4a68ff8758
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-30 09:21:30 +0530

    added mew modal: MembersModal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { Server } from "@prisma/client";
 
-export type ModalType = "createServer" | "invite" | "editServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members";
 
 interface ModalData {
   server?: Server;
