6cd8c33988310882fbc023c8876d5106cdd3f5f7
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-30 09:52:42 +0530

    render member name img and role with email

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -9,9 +9,17 @@ import {
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
+};
 
 export function MembersModal() {
   const { isOpen, onOpen, onClose, type, data } = useModal();
@@ -21,7 +29,7 @@ export function MembersModal() {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
@@ -32,10 +40,16 @@ export function MembersModal() {
         </DialogHeader>
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
           {server?.members?.map((member) => (
-            <div
-              key={member.id}
-              className="flex items-center gap-x-2 mb-6"
-            ></div>
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+            </div>
           ))}
         </ScrollArea>
       </DialogContent>
---
+++ components/user-avatar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+interface UserAvatarProps {
+  src?: string;
+  className?: string;
+}
+
+export function UserAvatar({ src, className }: UserAvatarProps) {
+  return (
+    <Avatar>
+      <AvatarImage
+        src={src}
+        className={cn("h-7 w-7 md:h-10 md:w-10", className)}
+      />
+    </Avatar>
+  );
+}
