d416741f7c219e1e9b849a13989e0057cb9578d4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-31 19:46:18 +0530

    new moda: DeleteChannelModal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { ChannelType, Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 
 export type ModalType =
   | "createServer"
@@ -8,10 +8,12 @@ export type ModalType =
   | "members"
   | "createChannel"
   | "leaveServer"
-  | "deleteServer";
+  | "deleteServer"
+  | "deleteChannel";
 
 interface ModalData {
   server?: Server;
+  channel?: Channel;
   channelType?: ChannelType;
 }
 
