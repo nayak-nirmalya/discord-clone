abc857d5a63cb2859b7ca5b078481e0e5e72bd5d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-31 19:41:50 +0530

    bug fix: siderbar nav clr on light theme

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -27,7 +27,7 @@ export async function NavigationSidebar() {
   });
 
   return (
-    <div className="space-y-4 flex flex-col h-full items-center text-primary w-full dark:bg-[#1e1f22] py-3">
+    <div className="space-y-4 flex flex-col h-full items-center text-primary w-full dark:bg-[#1e1f22] bg-[#e3e5e8] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
