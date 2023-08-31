8994179a869fe4a52acf93f3d49b98fdd7e6cb0b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-31 10:15:13 +0530

    open create channel modal on clikc

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -2,7 +2,11 @@
 
 import React from "react";
 import { ChannelType, MemberRole } from "@prisma/client";
+import { Plus } from "lucide-react";
+
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
   label: string;
@@ -19,11 +23,23 @@ export function ServerSection({
   role,
   server
 }: ServerSectionProps) {
+  const { onOpen } = useModal();
+
   return (
     <div className="flex items-center justify-between py-2">
       <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
         {label}
       </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
     </div>
   );
 }
