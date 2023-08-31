66524a47f00d13252703b6ff598c9035e53903b0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-31 12:59:04 +0530

    render audio and video channels on sidebar

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -137,6 +137,42 @@ export async function ServerSidebar({ serverId }: { serverId: string }) {
             ))}
           </div>
         )}
+        {!!audioChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Voice Channels"
+            />
+            {audioChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
+        {!!videoChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Video Channels"
+            />
+            {videoChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
