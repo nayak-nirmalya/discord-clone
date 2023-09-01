37d1a0eb3f2a53dcb7d462e527aecbb57e36128b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-01 20:15:09 +0530

    new chat component: ChatHeader

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function ChannelIdPage() {
-  return <div>ChannelIdPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+export default async function ChannelIdPage({
+  params: { channelId, serverId }
+}: ChannelIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: { id: channelId }
+  });
+
+  const members = await db.member.findFirst({
+    where: { serverId: serverId, profileId: profile.id }
+  });
+
+  if (!channel || !members) return redirect("/");
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader />
+    </div>
+  );
 }
---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ChatHeader() {
+  return <div>ChatHeader</div>;
+}
