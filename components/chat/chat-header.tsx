d049a74b6d67e1a4676c217a4f373d784049fa4a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-01 21:14:11 +0530

    open sidebar sheen on mobile view

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Hash, Menu } from "lucide-react";
 
+import { MobileToggle } from "@/components/mobile-toggle";
+
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -16,7 +18,7 @@ export function ChatHeader({
 }: ChatHeaderProps) {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu />
+      <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { Menu } from "lucide-react";
 
-export function MobileToggle() {
-  return <Menu />;
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+export function MobileToggle({ serverId }: { serverId: string }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
 }
