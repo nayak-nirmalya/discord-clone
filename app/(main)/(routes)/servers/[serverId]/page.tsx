488b053b89a6fc854ceb16442a7af51b24a7faed
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-01 12:53:51 +0530

    redirect to general chanel when click on server

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function ServerIdPage() {
-  return <div>ServerIDPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general"
+        },
+        orderBy: { createdAt: "asc" }
+      }
+    }
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
