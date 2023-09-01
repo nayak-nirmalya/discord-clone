04a5cb6dbe6e99204b8af440ab84dfc70c321691
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-01 12:23:07 +0530

    added api route PATCH /api/channels/id

--- app/api/channels/[channelId]/route.tsx
+++ app/api/channels/[channelId]/route.tsx
@@ -4,6 +4,64 @@ import { MemberRole } from "@prisma/client";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+    if (!serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+    if (!params.channelId)
+      return new NextResponse("Channel ID Missing", { status: 400 });
+
+    const { name, type } = await req.json();
+    if (!name || !type || name === "general")
+      return new NextResponse("Name / Type cannot be empty or general", {
+        status: 400
+      });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: "general"
+              }
+            },
+            data: {
+              name,
+              type
+            }
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[CHANNEL_ID_PATCH", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: { channelId: string } }
--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -76,7 +76,7 @@ export function EditChannelModal() {
         query: { serverId: server?.id }
       });
 
-      await axios.post(url, values);
+      await axios.patch(url, values);
 
       form.reset();
       router.refresh();
