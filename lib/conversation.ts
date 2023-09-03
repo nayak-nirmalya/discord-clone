8c58ef210e555f761f1699c189648ae31a534781
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-03 10:25:38 +0530

    added createNewConversation lib function

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,17 +1,63 @@
 import { db } from "@/lib/db";
 
-const findConversation = async (memberOneId: string, memberTwoId: string) => {
-  return await db.conversation.findFirst({
-    where: {
-      AND: [{ memberOneId }, { memberTwoId }]
-    },
-    include: {
-      memberOne: {
-        include: { profile: true }
+export const getOrCreateConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId);
+  }
+
+  return conversation;
+};
+
+const findConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  try {
+    return await db.conversation.findFirst({
+      where: {
+        AND: [{ memberOneId }, { memberTwoId }]
       },
-      memberTwo: {
-        include: { profile: true }
+      include: {
+        memberOne: {
+          include: { profile: true }
+        },
+        memberTwo: {
+          include: { profile: true }
+        }
       }
-    }
-  });
+    });
+  } catch (error) {
+    return null;
+  }
+};
+
+const createNewConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId
+      },
+      include: {
+        memberOne: {
+          include: { profile: true }
+        },
+        memberTwo: {
+          include: { profile: true }
+        }
+      }
+    });
+  } catch (error) {
+    return null;
+  }
 };
