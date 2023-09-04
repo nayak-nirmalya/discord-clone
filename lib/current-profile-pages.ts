a691bedf5f403132d84ee6ae4659637f1eb5bd52
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-04 12:44:20 +0530

    added lb fn currentProfile for page route api

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,16 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { NextApiRequest } from "next";
+
+import { db } from "@/lib/db";
+
+export const currentProfile = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) return null;
+
+  const profile = await db.profile.findUnique({
+    where: { userId }
+  });
+
+  return profile;
+};
