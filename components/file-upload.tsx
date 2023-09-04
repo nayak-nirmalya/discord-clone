754be20d96eb2c6c8b0f56baba37187bf35b4139
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-04 14:49:55 +0530

    added preview/delete for pdf in file upload

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "@uploadthing/react/styles.css";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -13,7 +13,11 @@ interface FileUploadProps {
   endpoint: "messageFile" | "serverImage";
 }
 
-export function FileUpload({ onChange, value, endpoint }: FileUploadProps) {
+export function FileUpload({
+  onChange,
+  value,
+  endpoint
+}: FileUploadProps) {
   const fileType = value?.split(".").pop();
 
   if (value && fileType !== "pdf") {
@@ -23,6 +27,29 @@ export function FileUpload({ onChange, value, endpoint }: FileUploadProps) {
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
