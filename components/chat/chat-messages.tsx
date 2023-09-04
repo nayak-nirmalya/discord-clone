19e765857af15cecff3034c6324ccd10f391487d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-04 19:57:32 +0530

    show loader if status 'loading'

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import { Member } from "@prisma/client";
+import { Loader2 } from "lucide-react";
 
 import { ChatWelcome } from "@/components/chat/chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
 
 interface ChatMessagesProps {
   name: string;
@@ -28,6 +30,25 @@ export function ChatMessages({
   paramValue,
   type
 }: ChatMessagesProps) {
+  const queryKey = `chat:${chatId}`;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue
+    });
+
+  if (status === "loading")
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    );
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
