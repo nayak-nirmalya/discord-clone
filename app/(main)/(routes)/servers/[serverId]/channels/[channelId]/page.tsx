f5dfdc5e7021b71bf6fb80ccc0ee1a69b719df13
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-04 12:10:58 +0530

    new component: ChatInput

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -37,6 +38,8 @@ export default async function ChannelIdPage({
         serverId={channel.serverId}
         type="channel"
       />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput />
     </div>
   );
 }
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,7 @@
+"use cliet";
+
+import React from "react";
+
+export function ChatInput() {
+  return <div>ChatInput</div>;
+}
