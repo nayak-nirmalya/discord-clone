e5cadc8e368dc03b9364a4f62fb7f92adc9915e3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-05 17:50:17 +0530

    show msg image if any

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React from "react";
-import { Member, Profile } from "@prisma/client";
+import { Member, MemberRole, Profile } from "@prisma/client";
 import { ShieldAlert, ShieldCheck } from "lucide-react";
+import Image from "next/image";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { ActionTooltip } from "@/components/action-tooltip";
@@ -38,6 +39,16 @@ export function ChatItem({
   socketUrl,
   socketQuery
 }: ChatItemProps) {
+  const fileType = fileUrl?.split(".").pop();
+
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.role === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
+
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-center w-full">
@@ -54,7 +65,25 @@ export function ChatItem({
                 {roleIconMap[member.role]}
               </ActionTooltip>
             </div>
+            <span className="text-xs text-zinc-500 dark:text-zinc-400">
+              {timestamp}
+            </span>
           </div>
+          {isImage && (
+            <a
+              href={fileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                className="object-cover"
+              />
+            </a>
+          )}
         </div>
       </div>
     </div>
